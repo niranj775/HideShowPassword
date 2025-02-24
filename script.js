0d47cadafd84d1201169acc49e39b44f2797cc2c
niranj775 <niranj775@gmail.com>
2025-02-24 21:22:27 +0530

    Final App

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Test");
+let eyeIcon = document.getElementById("eye-icon");
+let password = document.getElementById("password");
+
+eyeIcon.onclick = function(){
+  if(password.type == "password"){
+    password.type = "text";
+    eyeIcon.src = "eye-icons/eye-open.png";
+  }   
+  else {
+    password.type = "password";
+    eyeIcon.src = "eye-icons/eye-open.png"
+  } 
+}
